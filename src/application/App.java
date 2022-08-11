5e2c7647adf8229214fb73ff94de93d1e3285b1a
earl-stephens <stephensearl@hotmail.com>
2022-08-10 21:56:39 -0600

    Read back parts of a class file

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,36 @@
 package application;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String classFile = "/Users/earltstephens/eclipse-workspace/UdemyPractice/ClassFileExercise/bin/application/App.class";
+		
+		File file = new File(classFile);
+		
+		if(!file.exists()) {
+			System.out.println("File does not exist: " + classFile);
+		}
 
+		try(var dis = new DataInputStream(new FileInputStream(file))) {
+			int magicNumber = dis.readInt();
+			System.out.printf("%x\n", magicNumber);
+			
+			int minorVersion = dis.readUnsignedShort();
+			int majorVersion = dis.readUnsignedShort();
+			
+			System.out.println(majorVersion + ", " + minorVersion);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Completed.");
 	}
 
 }
